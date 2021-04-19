06a591813bb9462660710caa6691f7a70307c489
wjsdusdk <wjsdk1570@gmail.com>
2021-04-20 04:23:38 +0900

    프로젝트 생성

---
+++ main.js
@@ -0,0 +1,6 @@
+import _ from "lodash";
+
+console.log("hello world"); // hello world
+console.log(_.camelCase("hello world")); // helloWorld
+// lodash 모듈 안에 있는 camelCase 명령어를 사용
+// hello world가 camelCase 방식으로 변경이 되서 반환
